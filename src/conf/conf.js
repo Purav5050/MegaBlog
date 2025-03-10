85c11d2f5b08aa4e17957cb81ec872a95ad302fc
Purav5050 <164227956+Purav5050@users.noreply.github.com>
2025-03-11 01:35:00 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    appwriteUrl: "https://cloud.appwrite.io/v1",
-    appwriteProjectId: "67c05f500014e1020fab",
-    appwriteDatabaseId:"67c05ffc00092a2114d3",
-    appwriteCollectionId:"67c0603400209424ac18",
-    appwriteBucketId: "67c061bd002dd3ab7b94",
+    appwriteUrl: "",
+    appwriteProjectId: "",
+    appwriteDatabaseId:"",
+    appwriteCollectionId:"",
+    appwriteBucketId: "",
 }
 // there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
 
